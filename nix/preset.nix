7a9871809d0db994d4e7f2f0903977e0828d4e1d
comavius <comavius@proton.me>
2025-10-23 18:59:20 +0900

    🚚 move nix lib to root

---
+++ nix/preset.nix
@@ -0,0 +1,15 @@
+{
+  shadertoyGlobals = {
+    time = "iTime";
+    resolution = "iResolution";
+    mouse = "iMouse";
+    position = "iPosition";
+  };
+
+  theBookOfShaders = {
+    time = "u_time";
+    resolution = "u_resolution";
+    mouse = "u_mouse";
+    position = "a_position";
+  };
+}
--- src/rendering_pipeline_builder/conf_reader/tests.rs
+++ src/rendering_pipeline_builder/conf_reader/tests.rs
@@ -5,8 +5,8 @@ use super::*;
 fn test_read_rendering_pipeline_configuration_from_nix_file() {
     let here = file!();
     let here_path = PathBuf::from(here).parent().unwrap().to_path_buf();
-    let test_nix_file_path = here_path.join("test_nix").join("RenderingPipeline.nix");
-    let nix_lib_dir = here_path.join("nix");
+    let test_nix_file_path = here_path.join("test_nix").join("renderGraph.nix");
+    let nix_lib_dir = std::env::current_dir().unwrap().join("nix");
     let default_resolution = ConfResolution {
         height: 1080,
         width: 1920,
