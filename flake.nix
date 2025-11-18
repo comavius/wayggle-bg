4b3f4d38ad85703f1ec0d724c30fbe5b96f0ba47
comavius <comavius@proton.me>
2025-11-18 12:02:34 +0900

    add hakari and crane

--- flake.nix
+++ flake.nix
@@ -6,12 +6,14 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    crane,
     rust-overlay,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
@@ -26,33 +28,68 @@
         ];
       };
 
-      rustPlatform = pkgs.makeRustPlatform {
-        cargo = toolchain;
-        rustc = toolchain;
-      };
-
-      app-version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
-
       nativeBuildInputs = with pkgs; [
         toolchain
         pkg-config
+        libxkbcommon
       ];
 
       buildInputs = with pkgs; [
         libglvnd
         wayland
+        libxkbcommon
         shaderc
       ];
-    in {
-      packages.default = rustPlatform.buildRustPackage {
-        pname = "wayggle-bg";
-        version = app-version;
-        src = self;
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+      src = craneLib.cleanCargoSource ./.;
+
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+
+        inherit buildInputs nativeBuildInputs;
+
+        outputHashes = {
+          "git+https://git.snix.dev/snix/snix.git?rev=e33040a3e1a500e73dd8a4c2b9e793d7cb85384f#e33040a3e1a500e73dd8a4c2b9e793d7cb85384f" = "sha256-TpWEIhAgzGIupKARl+a3btrBaV9wQGYyxzN42Cnmu14=";
         };
-        inherit nativeBuildInputs buildInputs;
       };
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      individualCrateArgs =
+        commonArgs
+        // {
+          inherit cargoArtifacts;
+          inherit (craneLib.crateNameFromCargoToml {inherit src;}) version;
+          doCheck = false;
+        };
+
+      fileSetForCrate = crate:
+        pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = pkgs.lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            (craneLib.fileset.commonCargoSources ./crates/wayland_app)
+            (craneLib.fileset.commonCargoSources ./crates/core)
+            (craneLib.fileset.commonCargoSources crate)
+            ./crates/wayland_app/src/shader.wgsl
+          ];
+        };
+
+      wayggle-bg = craneLib.buildPackage (
+        individualCrateArgs
+        // {
+          pname = "wayggle-bg";
+          cargoExtraArgs = "-p wayggle-bg-app";
+          src = fileSetForCrate ./crates/wayland_app;
+        }
+      );
+    in {
+      packages.default = wayggle-bg;
+      packages.cargoArtifacts = cargoArtifacts;
+      packages.craneLib = src;
 
       devShells.default = pkgs.mkShell {
         packages =
@@ -60,6 +97,7 @@
           ++ nativeBuildInputs
           ++ [
             pkgs.xc
+            pkgs.cargo-hakari
           ];
       };
 
